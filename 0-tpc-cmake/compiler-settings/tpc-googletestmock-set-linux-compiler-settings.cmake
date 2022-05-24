b3e61feb333728ef4efd30989a11da3bdad11c24
jiteman <jiteman.ru@ya.ru>
2022-05-24 18:29:51 +0300

    initial main files has been added

---
+++ 0-tpc-cmake/compiler-settings/tpc-googletestmock-set-linux-compiler-settings.cmake
@@ -0,0 +1,18 @@
+JSD_CMAKE_CURRENT_FILE_IN( "${JSD_SSC_FULL_NAME}-linux-compiler-settings.cmake" )
+
+if ( UNIX )
+	## set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+
+	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS "-W" ) #
+
+	## string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS}" )
+
+	## string( REPLACE "-W" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) #
+
+	## set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
+else()
+	message( SEND_ERROR "[JSD] ${JSD_SSC_FULL_NAME} COMPILER SETTINGS: ${JSD_SSC_FULL_NAME}-linux-compiler-settings.cmake is included while not on linux" )
+
+endif()
+
+JSD_CMAKE_CURRENT_FILE_OUT( "${JSD_SSC_FULL_NAME}-linux-compiler-settings.cmake" )
