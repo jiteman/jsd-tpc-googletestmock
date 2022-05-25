e7c61a83b48791b10ccb5f081e3e8e3ef67f9c42
jiteman <jiteman.ru@ya.ru>
2022-05-25 13:48:24 +0300

    configuration has been updated

--- 0-tpc-cmake/compiler-settings/tpc-googletestmock-set-linux-compiler-settings.cmake
+++ 0-tpc-cmake/compiler-settings/tpc-googletestmock-set-linux-compiler-settings.cmake
@@ -1,15 +1,33 @@
 JSD_CMAKE_CURRENT_FILE_IN( "${JSD_SSC_FULL_NAME}-linux-compiler-settings.cmake" )
 
 if ( UNIX )
-	## set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+	set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
 
-	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS "-W" ) #
+	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS "-Wno-" ) #
 
-	## string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS}" )
+	string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS}" )
 
 	## string( REPLACE "-W" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) #
+	string( REPLACE "-Wundef" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) # "" is not defined, evaluates to 0
+	string( REPLACE "-Wmissing-declarations" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) # no previous declaration for ""
 
-	## set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
+	set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
+
+	## set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-format" )
+
+	# release
+	set( ${PROJECT_NAME}_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_RELEASE} )
+
+	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS_RELEASE "-Wno-" ) #
+	list( APPEND ${PROJECT_NAME}_CXX_FLAGS_RELEASE "-finline-limit=1000" ) # # default ( 600 )
+	list( APPEND ${PROJECT_NAME}_CXX_FLAGS_RELEASE "--param inline-unit-growth=100" ) # default ( 50 )
+
+	string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_RELEASE_STR "${${PROJECT_NAME}_CXX_FLAGS_RELEASE}" )
+
+	## string( REPLACE "-W" "" ${PROJECT_NAME}_CXX_FLAGS_RELEASE_STR "${${PROJECT_NAME}_CXX_FLAGS_RELEASE_STR}" ) #
+
+	set( CMAKE_CXX_FLAGS_RELEASE ${${PROJECT_NAME}_CXX_FLAGS_RELEASE_STR} )
+
 else()
 	message( SEND_ERROR "[JSD] ${JSD_SSC_FULL_NAME} COMPILER SETTINGS: ${JSD_SSC_FULL_NAME}-linux-compiler-settings.cmake is included while not on linux" )
 
